24748e6f51e92aff6cae1c16fc34fde72f97d2b0
suranjanmaity <suranjanmaity4@gmail.com>
2023-03-31 23:37:31 +0530

    8 programs done

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ buttons[1].addEventListener("click", ()=>{
     }
 }
 );
+//! reverse
 buttons[2].addEventListener("click", ()=>{
     let num = prompt("enter any number to reverse it.",0);
     console.log();
@@ -37,18 +38,109 @@ buttons[2].addEventListener("click", ()=>{
     alert(`Reverse of ${num} is ${rev}`)
 }
 );
+//! factorial
+let factorial = (num) => {
+    let fact=1;
+    for(let i=2; i<=num; i++){
+        fact=fact*i;
+    }
+    return fact;
+};
+// ! strong number
 buttons[3].addEventListener("click", ()=>{
     let num = prompt("enter any number to reverse it.",0);
-    console.log();
     let arr = num.split("");
-    let rev = "";
+    let sum = 0;
     arr.map((value)=>{
-        rev = value + rev ;
+        sum=sum+factorial( parseInt( value ) );
     })
-    alert(`Reverse of ${num} is ${rev}`)
+    if (sum===parseInt(num)) {
+        alert(num+" is strong");
+    } else {
+        alert(num+" is not strong");
+    }
+    // 145 = 1 + 4! + 5!
+    // 1!               =   1
+    // 4! = 4*3*2*1     =  24
+    // 4! = 5*4*3*2*1   = 120
+    //                  = 145
 }
 );
-
-// ...rest = [1,2,3,4]
-
-// num = [0, ...spread]
+//! pattern
+// *
+// **
+// ***
+// ****
+buttons[4].addEventListener("click", ()=>{
+    let num = parseInt( prompt("enter any number to console pattern",0) );
+    for (let i = 0; i < num; i++) {
+        let str="";
+        for (let j = 0; j < i; j++) {
+            str=str+"* ";
+        }
+        console.log(str);
+    }
+    
+}
+);
+//! pattern
+// ****
+// ***
+// **
+// *
+buttons[5].addEventListener("click", ()=>{
+    let num = parseInt( prompt("enter any number to console pattern",0) );
+    for (let i = num; i > 0; i--) {
+        let str="";
+        for (let j = 0; j < i; j++) {
+            str=str+"* ";
+        }
+        console.log(str);
+    }
+    
+}
+);
+//! pattern
+// ****
+//  ***
+//   **
+//    *
+buttons[6].addEventListener("click", ()=>{
+    let num = parseInt( prompt("enter any number to console pattern",0) );
+    for (let i = num; i > 0; i--) {
+        let str="";
+        // for space
+        for (let k = 0; k < num-i; k++) {
+            str=str+"  ";
+        }
+        // for star
+        for (let j = 0; j < i; j++) {
+            str=str+"* ";
+        }
+        console.log(str);
+    }
+    
+}
+);
+//! pattern
+//    *
+//   **
+//  ***
+// ****
+buttons[7].addEventListener("click", ()=>{
+    let num = parseInt( prompt("enter any number to console pattern",0) );
+    for (let i = 1; i <= num; i++) {
+        let str="";
+        // for space
+        for (let k = num-i ; k > 0 ; k--) {
+            str=str+"  ";
+        }
+        // for star
+        for (let j = 1; j <= i; j++) {
+            str=str+"* ";
+        }
+        console.log(str);
+    }
+    
+}
+);
