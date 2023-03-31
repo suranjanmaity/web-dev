dfec39fdfadbf29f4916544be52b90b3dac47c16
suranjanmaity <suranjanmaity4@gmail.com>
2023-03-31 19:08:25 +0530

    questions

--- Questions.md
+++ Questions.md
@@ -4,3 +4,27 @@ object
 destructuring
 rest and spread operator
 promise
+
+
+what is prototype
+ways to center a div
+psuedo selector
+psuedo element selector
+types of datatype in js
+promise
+states of promise
+	pending
+	fullfilled
+	rejected
+handlers of promise
+	then
+	catch
+	finally
+what is async await
+what is callback
+	a function that is passed as argument or returned from another function
+splice vs slice
+	splice modify original array
+	slice make copy original array
+what is string
+	
--- js/oops.js
+++ js/oops.js
@@ -33,3 +33,52 @@ let human = new Human("chombi");
 console.log(human);
 console.log(human._hello());
 // console.log(human.#hello()); // will give error message
+
+class NewHuman{
+    constructor(name){
+        this.name = name;
+    }
+    // ! Static data member
+    static speak(){
+        console.log(this.name);
+    };
+    // static data members were not accessible by instance
+    // because static data members are not added to prototype of class
+}
+let h1 = new NewHuman("chomb");
+let h2 = new NewHuman("chombi");
+console.log(h1);
+console.log(h2);
+// console.log(h2.speak());    // will get Uncaught TypeError: h2.speak is not a function
+console.log(NewHuman.speak());
+// we can access static members with the help of class name
+
+class NormalHuman{
+    constructor(name){
+        this.name = name;
+    }
+    brain(){
+        console.log("brain available");
+    }
+    speak(){
+        console.log("I'm human, My name is " + this.name);
+    }
+}
+// ? 'extends' keyword is used to inherit from all public members from parent class
+class SuperHuman extends NormalHuman{
+    constructor(sname) {
+        // ? 'super' keyword is used to call constructor from parent class
+        super(sname);
+    }
+    fly(){
+        console.log("i can fly");
+    }
+    heat(){
+        console.log("heat blast");
+    }
+}
+let superHuman = new SuperHuman("CHAMP");
+superHuman.speak();
+superHuman.brain();
+superHuman.fly();
+superHuman.heat();
